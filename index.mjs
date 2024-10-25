590a76fe69ad6e5e79b1a3276a294ff18d37da75
LyndaFitz <lyndaa.fitz@gmail.com>
2024-10-25 10:23:45 -0400

    all parts inside

---
+++ index.mjs
@@ -0,0 +1,7 @@
+export function square(num) {
+    return num * num;
+}
+
+// Example usage:
+console.log(square(5)); // Output: 25
+console.log(square(10)); // Output: 100
---
+++ script.mjs
@@ -0,0 +1,7 @@
+import { n } from "./main.mjs"; 
+
+import { square } from "./index.mjs";
+
+console.log(n)
+
+console.log(square(2))
